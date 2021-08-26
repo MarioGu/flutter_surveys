71268f13b6c030f32fdb05868f0019048048ccf8
Mario Bonaldi <mario.bonaldi@gmail.com>
2021-08-26 16:46:34 -0300

    test: ensure RemoteAuthentication returns an Acoount on 200

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -1,4 +1,5 @@
 import '../../domain/helpers/domain_error.dart';
+import '../../domain/entities/entities.dart';
 import '../../domain/usecases/usecases.dart';
 
 import '../http/http.dart';
@@ -12,14 +13,16 @@ class RemoteAuthentication {
     required this.url,
   });
 
-  Future<void> auth(AuthenticationParams params) async {
+  Future<AccountEntity> auth(AuthenticationParams params) async {
     final body = RemoteAuthenticationParams.fromDomain(params).toJson();
     try {
-      await httpClient.request(url: url, method: 'post', body: body);
-    } on HttpError catch(error) {
-      throw error == HttpError.unauthorized 
-        ? DomainError.invalidCredentials
-        : DomainError.unexpected;
+      final httpResponse =
+          await httpClient.request(url: url, method: 'post', body: body);
+      return AccountEntity.fromJson(httpResponse);
+    } on HttpError catch (error) {
+      throw error == HttpError.unauthorized
+          ? DomainError.invalidCredentials
+          : DomainError.unexpected;
     }
   }
 }
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -28,6 +28,13 @@ void main() {
   });
 
   test('Should call HttpClient with correct values', () async {
+    when(() =>
+        httpClient.request(
+            url: any(named: 'url'),
+            method: any(named: 'method'),
+            body: any(named: 'body'))).thenAnswer(
+        (_) async => {'accessToken': faker.guid.guid(), 'name': faker.person.name()});
+
     await sut.auth(params);
 
     verify(() => httpClient.request(
@@ -75,7 +82,8 @@ void main() {
     expect(future, throwsA(DomainError.unexpected));
   });
 
-  test('Should throw InvalidCredentialsError if HttpClient returns 401', () async {
+  test('Should throw InvalidCredentialsError if HttpClient returns 401',
+      () async {
     when(() => httpClient.request(
         url: any(named: 'url'),
         method: any(named: 'method'),
@@ -86,5 +94,25 @@ void main() {
     final future = sut.auth(params);
 
     expect(future, throwsA(DomainError.invalidCredentials));
+  });
+
+  test('Should return an Account if HttpClient returns 200', () async {
+    final accessToken = faker.guid.guid();
+
+    when(() =>
+        httpClient.request(
+            url: any(named: 'url'),
+            method: any(named: 'method'),
+            body: any(named: 'body'))).thenAnswer(
+        (_) async => {'accessToken': accessToken, 'name': faker.person.name()});
+
+    final params = AuthenticationParams(
+        email: faker.internet.email(), password: faker.internet.password());
+
+    final account = await sut.auth(params);
+
+    expect(
+      account.token, accessToken
+    );
   });
 }
