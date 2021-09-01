fbcffda00aa7463436ee939f525f036ee7587202
Mario Bonaldi <mario.bonaldi@gmail.com>
2021-09-01 19:33:30 -0300

    refactor: move Validation to its own file

--- test/presentation/presenters/streaming_login_presenter_test.dart
+++ test/presentation/presenters/streaming_login_presenter_test.dart
@@ -4,9 +4,7 @@ import 'package:faker/faker.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
-abstract class Validation {
-  String? validate({required String field, required String value}) {}
-}
+import 'package:flutter_course/presentation/protocols/validation.dart';
 
 class LoginState {
   late String? emailError;
