1ffe6d4cce3e0c6982420ae4619f83e84d2b6f48
Mario Bonaldi <mario.bonaldi@gmail.com>
2021-09-03 09:27:27 -0300

    test: ensure LoginValitionsFatory returns correct validations

---
+++ test/main/factories/pages/login/login_validation_factory_test.dart
@@ -0,0 +1,16 @@
+import 'package:test/test.dart';
+
+import 'package:flutter_course/validation/validators/validators.dart';
+import 'package:flutter_course/main/factories/pages/pages.dart';
+
+void main() {
+  test('Should return the correct values', () {
+    final validations = makeLoginValidations();
+
+    expect(validations, [
+      const RequiredFieldValidation('email'),
+      const EmailValidation('email'),
+      const RequiredFieldValidation('password'),
+    ]);
+  });
+}
