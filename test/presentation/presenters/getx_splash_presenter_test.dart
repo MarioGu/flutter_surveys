4c2eac85cdc3856ccf3b3f6d6623a02e192b8581
Mario Bonaldi <mario.bonaldi@gmail.com>
2021-09-08 18:18:45 -0300

    test: ensure GetxSplashPresenter emits surveys page on success

--- test/presentation/presenters/getx_splash_presenter_test.dart
+++ test/presentation/presenters/getx_splash_presenter_test.dart
@@ -18,6 +18,7 @@ class GetxSplashPresenter implements SplashPresenter {
   @override
   Future<void> checkAccount() async {
     await loadCurrentAccount.load();
+    _navigateTo.value = '/surveys';
   }
 }
 
@@ -45,5 +46,12 @@ void main() {
     await sut.checkAccount();
 
     verify(loadCurrentAccount.load).called(1);
+  });
+
+  test('Should go to surveys page on success', () async {
+    sut.navigateToStream
+        .listen(expectAsync1((page) => expect(page, '/surveys')));
+
+    await sut.checkAccount();
   });
 }
