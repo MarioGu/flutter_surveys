13b5a3d076082f531241932d12a820e3ca81ec29
Mario Bonaldi <mario.bonaldi@gmail.com>
2021-09-08 14:32:04 -0300

    test: ensure SplashPage calls loadCurrentAccount on page load

--- test/ui/pages/splash_page_test.dart
+++ test/ui/pages/splash_page_test.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/route_manager.dart';
+import 'package:mocktail/mocktail.dart';
 
 class SplashPage extends StatelessWidget {
-  const SplashPage({Key? key}) : super(key: key);
+  final SplashPresenter presenter;
+
+  const SplashPage({Key? key, required this.presenter}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    presenter.loadCurrentAccount();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Chanllenges'),
@@ -19,11 +24,31 @@ class SplashPage extends StatelessWidget {
   }
 }
 
+abstract class SplashPresenter {
+  Future<void> loadCurrentAccount();
+}
+
+class SplashPresenterSpy extends Mock implements SplashPresenter {}
+
 void main() {
+  late SplashPresenterSpy presenter;
+
+  When mockLoadCurrentAccountCall(SplashPresenter presenter) {
+    return when(() => presenter.loadCurrentAccount());
+  }
+
+  void mockLoadCurrentAccount(SplashPresenter presenter) {
+    mockLoadCurrentAccountCall(presenter).thenAnswer((_) async => null);
+  }
+
   Future<void> loadPage(WidgetTester tester) async {
+    presenter = SplashPresenterSpy();
+    mockLoadCurrentAccount(presenter);
     await tester.pumpWidget(GetMaterialApp(
       initialRoute: '/',
-      getPages: [GetPage(name: '/', page: () => const SplashPage())],
+      getPages: [
+        GetPage(name: '/', page: () => SplashPage(presenter: presenter))
+      ],
     ));
   }
 
@@ -32,5 +57,12 @@ void main() {
     await loadPage(tester);
 
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('Should call loadCurrentAccount on page load',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    verify(() => presenter.loadCurrentAccount()).called(1);
   });
 }
