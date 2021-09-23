5e7c992659df8c8393115d9e5228de6223a65c2b
Mario Bonaldi <mario.bonaldi@gmail.com>
2021-09-23 18:53:59 -0300

    test: ensure GetxSignUpPresenter emits correct error on name change

--- test/presentation/presenters/getx_signup_presenter_test.dart
+++ test/presentation/presenters/getx_signup_presenter_test.dart
@@ -13,6 +13,7 @@ void main() {
   late GetxSignUpPresenter sut;
   late ValidationSpy validation;
   late String email;
+  late String name;
 
   setUp(() {
     validation = ValidationSpy();
@@ -20,6 +21,7 @@ void main() {
       validation: validation,
     );
     email = faker.internet.email();
+    name = faker.person.name();
   });
 
   When mockValidationCall(String? field) => when(() => validation.validate(
@@ -69,5 +71,47 @@ void main() {
 
     sut.validateEmail(email);
     sut.validateEmail(email);
+  });
+
+  test('Should call Validation with correct name', () {
+    sut.validateName(name);
+
+    verify(() => validation.validate(field: 'name', value: name)).called(1);
+  });
+
+  test('Should emit invalid field error if name is invalid', () {
+    mockValidation(value: ValidationError.invalidField);
+
+    sut.nameErrorStream
+        .listen(expectAsync1((error) => expect(error, UIError.invalidField)));
+
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validateName(name);
+    sut.validateName(name);
+  });
+
+  test('Should emit invalid field error if name is empty', () {
+    mockValidation(value: ValidationError.requiredField);
+
+    sut.nameErrorStream
+        .listen(expectAsync1((error) => expect(error, UIError.requiredField)));
+
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validateName(name);
+    sut.validateName(name);
+  });
+
+  test('Should emit null if name validation succeds', () {
+    sut.nameErrorStream.listen(expectAsync1((error) => expect(error, null)));
+
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validateName(name);
+    sut.validateName(name);
   });
 }
