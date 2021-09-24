264344abeddc79ada4e5508bccaac2b27e03c6d1
Mario Bonaldi <mario.bonaldi@gmail.com>
2021-09-24 14:52:10 -0300

    ensure RemoteLoadSurveys calls HttpClient with correct values

---
+++ test/data/usecases/load_surveys/remote_load_surveys_test.dart
@@ -0,0 +1,47 @@
+import 'package:faker/faker.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+import 'package:flutter_course/data/http/http.dart';
+
+class HttpClientSpy extends Mock implements HttpClient {}
+
+class RemoteLoadSurveys {
+  HttpClient httpClient;
+  String url;
+
+  RemoteLoadSurveys({required this.httpClient, required this.url});
+
+  Future<void> load() async {
+    await httpClient.request(url: url, method: 'get');
+  }
+}
+
+void main() {
+  late HttpClientSpy httpClient;
+  late RemoteLoadSurveys sut;
+  late String url;
+
+  When<Future<Map<dynamic, dynamic>>> mockRequest() => when(() =>
+      httpClient.request(url: any(named: 'url'), method: any(named: 'method')));
+
+  void mockHttpData(Map data) {
+    mockRequest().thenAnswer((_) async => data);
+  }
+
+  setUp(() {
+    httpClient = HttpClientSpy();
+    url = faker.internet.httpUrl();
+    sut = RemoteLoadSurveys(httpClient: httpClient, url: url);
+    mockHttpData({});
+  });
+
+  test('Should call HttpClient with correct values', () async {
+    await sut.load();
+
+    verify(() => httpClient.request(
+          url: url,
+          method: 'get',
+        ));
+  });
+}
