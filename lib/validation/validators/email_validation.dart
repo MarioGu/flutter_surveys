455f8c24445c74219a577a78da78c31a205ca770
Mario Bonaldi <mario.bonaldi@gmail.com>
2021-09-24 07:54:31 -0300

    feat: improve EmailValidation regex

--- lib/validation/validators/email_validation.dart
+++ lib/validation/validators/email_validation.dart
@@ -16,7 +16,7 @@ class EmailValidation extends Equatable implements FieldValidation {
   @override
   ValidationError? validate(Map input) {
     final regex = RegExp(
-        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
     final isValid =
         input[field]?.isNotEmpty != true || regex.hasMatch(input[field]!);
     return isValid ? null : ValidationError.requiredField;
