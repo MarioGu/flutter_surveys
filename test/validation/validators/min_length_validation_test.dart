81be49de57b1d023f8bd8fc2d8a0d2837158e4a8
Mario Bonaldi <mario.bonaldi@gmail.com>
2021-09-23 22:26:19 -0300

    test: ensure MinLengthValidation returns null if value is equal to min size

--- test/validation/validators/min_length_validation_test.dart
+++ test/validation/validators/min_length_validation_test.dart
@@ -13,6 +13,9 @@ class MinLengthValidation implements FieldValidation {
 
   @override
   ValidationError? validate(String? value) {
+    if (value?.length == size) {
+      return null;
+    }
     return ValidationError.invalidField;
   }
 }
@@ -35,5 +38,9 @@ void main() {
   test('Should return error if value less than min size', () {
     expect(sut.validate(faker.randomGenerator.string(4, min: 1)),
         ValidationError.invalidField);
+  });
+
+  test('Should return null if value is equal to min size', () {
+    expect(sut.validate(faker.randomGenerator.string(5, min: 5)), null);
   });
 }
